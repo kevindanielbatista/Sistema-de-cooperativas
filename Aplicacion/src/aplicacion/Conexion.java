df4d37bfc39f04987febdadb25d0c98b0f85c653
Bibi <kevinfiguereo40@gmail.com>
2023-05-11 22:12:04 -0400

    Error de apellido y cedula corregido

--- Aplicacion/src/aplicacion/Conexion.java
+++ Aplicacion/src/aplicacion/Conexion.java
@@ -35,7 +35,7 @@ public class Conexion {
     
     //Usamos una funcion publica para insertar datos que toma todos los datos necesarios para cada columna
     public static void insertarDatos(String nombre, String cedula, String apellidos, int dia_nacimiento, String mes_nacimiento, int ano_nacimiento, String sexo, String est_civil, String correo, String telefono, String direccion) {
-        String sql = "INSERT INTO miembros(nombre,cedula,apellidos,dia_nacimiento,mes_nacimiento,ano_nacimiento,sexo,est_civil,correo,telefono,direccion) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO miembros_ahorro(nombre,cedula,apellidos,dia_nacimiento,mes_nacimiento,ano_nacimiento,sexo,est_civil,correo,telefono,direccion) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
 
         //Aqui se insertan los datos uno por uno en cada columna
         try (Connection conn = connect();
